10472c5986a3992fc91d388982f96102ce43aaaa
Bjorn T. Nostdahl <bjorn.nostdahl@gunnebo.com>
2025-07-10 19:57:37 +0200

    BT

--- src/main.py
+++ src/main.py
@@ -7,21 +7,6 @@ from dmx import DMX
 from Prolights_LumiPar7UTRI_8ch import Prolights_LumiPar7UTRI_8ch
 
 
-def blink_red(send: Callable[[Dict[int, int]], None], *, start_address: int = 1,
-              blink_times: int = 5, interval: float = 0.2) -> None:
-    """Blink fixture red a number of times."""
-    on_frame = {
-        start_address: 255,
-        start_address + 1: 0,
-        start_address + 2: 0,
-        start_address + 6: 255,
-    }
-    off_frame = {start_address + 6: 0}
-    for _ in range(blink_times):
-        send(on_frame)
-        time.sleep(interval)
-        send(off_frame)
-        time.sleep(interval)
 
 
 def main() -> None:
